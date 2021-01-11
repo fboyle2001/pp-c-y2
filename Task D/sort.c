ce0f14adfc7cc6ad875fc778b5bbd1d132b89454
fboyle2001 <finlayboyle2001@gmail.com>
2021-01-11 11:41:48 +0000

    strcoll over strcmp

--- Task D/sort.c
+++ Task D/sort.c
@@ -169,9 +169,9 @@ int main(int argc, char** argv) {
 }
 
 int compareStrings(const void *a, const void *b) {
-  // Just use strcmp to do the comparison
+  // Just use strcoll to do the comparison. Prefer strcoll over strcmp as it respects locale
   // the cast is void *a -> char **a then dereference char **a to get the char* which is the string
-  return strcmp(*(char **)a, *(char **)b);
+  return strcoll(*(char **)a, *(char **)b);
 }
 
 int compareStringsReverse(const void *a, const void *b) {
@@ -183,7 +183,7 @@ int compareStringsNumeric(const void *a, const void *b) {
   /*
   * 1. Extract the number from the front
   * 2. Compare them
-  * 3. If they are equal return strcmp(a, b) instead
+  * 3. If they are equal return strcoll(a, b) instead
   */
   int scannedNumberFromA;
   int matchedA = sscanf(*(char **)a, "%d", &scannedNumberFromA);
@@ -208,7 +208,7 @@ int compareStringsNumeric(const void *a, const void *b) {
       return -1;
     }
 
-    // Otherwise go to compare by strcmp like normal
+    // Otherwise go to compare by strcoll like normal
   }
 
   // A starts with a number but B doesn't
@@ -244,7 +244,7 @@ int compareStringsNumeric(const void *a, const void *b) {
 
   // otherwise compare normally
 
-  return strcmp(*(char **)a, *(char **)b);
+  return strcoll(*(char **)a, *(char **)b);
 }
 
 int compareStringsNumericReverse(const void *a, const void *b) {
